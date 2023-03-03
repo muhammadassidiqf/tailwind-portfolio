77554261e94fc937c2666f64fa8382805e4a5ce8
muhammadassidiqf <massidiqfattah@gmail.com>
2023-03-03 19:05:25 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors:{
+        'body': '#dfdfdf',
+        'header': '#232631'
+      }
+    },
+  },
+  plugins: [],
+}
